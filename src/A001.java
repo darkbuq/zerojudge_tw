529352b176c5f9e7082050fd6fe4ec517715e32a
chen yan rong <darkbuq@gmail.com>
2020-06-20 15:43:42 +0800

    A001 ok

--- src/A001.java
+++ src/A001.java
@@ -3,21 +3,7 @@ import java.util.Scanner;
 
 public class A001 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
-		int num1, num2, num3;
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("請輸入三個數字(以空白鍵分開)：");
-        num1 = scanner.nextInt();
-        num2 = scanner.nextInt();
-        num3 = scanner.nextInt();
-        System.out.println("您輸入第一個數字為：" + num1 );
-        System.out.println("您輸入第二個數字為：" + num2 );
-        System.out.println("您輸入第三個數字為：" + num3 );
-        */
-		
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
 		
 		while (sc.hasNext()) {
