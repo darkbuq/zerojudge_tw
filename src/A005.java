b9d645b46941dfff1be70f1f266934b5a7921a37
chen yan rong <darkbuq@gmail.com>
2020-06-21 22:18:44 +0800

    A005 ok

--- src/A005.java
+++ src/A005.java
@@ -12,11 +12,24 @@ public class A005 {
 			
 			String numl;
 			for (int i = 0; i<num; i++) {
-				//System.out.println("i= "+i);
+				
 				numl=sc.nextLine();
 				String[] numarr1 = numl.split(" ");
-				System.out.println(numarr1[2]);
-				//int[] numarr2 = new int[4];
+				
+				int a1 = Integer.parseInt(numarr1[0]);
+				int a2 = Integer.parseInt(numarr1[1]);
+				int a3 = Integer.parseInt(numarr1[2]);
+				int a4 = Integer.parseInt(numarr1[3]);
+				
+				if ( (a2-a1)==(a3-a2) ) {
+					int a5 = a4+(a3-a2);
+					numl=numl+" "+a5;
+					System.out.println(numl);
+				} else {
+					int a5 = a4*(a3/a2);
+					numl=numl+" "+a5;
+					System.out.println(numl);
+				}
 				
 			}
 			
